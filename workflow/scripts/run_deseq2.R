737a99948361a225d7f3a89efc6048b2b33a1ae9
FranciscoDegiovani <francisco.degiovani@outlook.com>
2026-07-26 22:48:46 -0300

    Implement DESeq2 analysis and result export

--- workflow/scripts/run_deseq2.R
+++ workflow/scripts/run_deseq2.R
@@ -1,1 +1,26 @@
+library(DESeq2)
+library(airway)
+library(apeglm)
 
+data(airway)
+dds <- DESeqDataSet(airway, design = ~ cell + dex)
+dds$dex <- relevel(dds$dex, ref = "untrt")
+
+keep <- rowSums(counts(dds)) >= 10
+dds <- dds[keep, ]
+
+dds <- DESeq(dds)
+res_shrunk <- lfcShrink(dds, coef = "dex_trt_vs_untrt", type = "apeglm")
+
+vsd <- vst(dds, blind = FALSE)
+
+# Save intermediate objects for the plotting step
+saveRDS(dds, snakemake@output[["dds"]])
+saveRDS(res_shrunk, snakemake@output[["res_shrunk"]])
+saveRDS(vsd, snakemake@output[["vsd"]])
+
+# Export results table
+res_export <- as.data.frame(res_shrunk)
+res_export$gene_id <- rownames(res_export)
+res_export <- res_export[order(res_export$padj), ]
+write.csv(res_export, snakemake@output[["csv"]], row.names = FALSE)
